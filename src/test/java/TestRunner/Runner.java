e2e134c3c153d2ebf0277194e72949dd12fb1d24
ahadjamshed420 <ahad.jamshed@gmail.com>
2023-08-22 01:20:08 +0500

    asdasd

---
+++ src/test/java/TestRunner/Runner.java
@@ -0,0 +1,22 @@
+package TestRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions
+        (features = "D://MyProject//Features//Login.feature",
+                glue = "StepDefination",
+                //tags = "@Sanity",
+                tags = "@Regression",
+                dryRun = false,
+                monochrome = false,
+                plugin = {"pretty", "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"}
+
+
+
+        )
+public class Runner {
+}
