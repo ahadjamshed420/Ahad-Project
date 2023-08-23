c6f8e9703685bf3073fc2ed9698b9a3430063b18
ahadjamshed420 <ahad.jamshed@gmail.com>
2023-08-23 17:18:11 +0500

    adding the relative path of feature file and log

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -6,10 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions
-        (features = "D://Automation Task//Ahad-Project//Features//Login.feature",
+        (features = "Features/Login.feature",
                 glue = "StepDefination",
                 //tags = "@Sanity",
-                tags = "@Regression",
+                //tags = "@Regression",
                 dryRun = false,
                 monochrome = false,
                 plugin = {"pretty", "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"}
